4d8dd10b83d06620f9cb516669c7a0c9b8aab679
sunyxl <39620856+elevenSevenxl@users.noreply.github.com>
2023-08-06 18:34:58 +0800

    Update config.ts

--- src/.vuepress/config.ts
+++ src/.vuepress/config.ts
@@ -3,7 +3,7 @@ import { searchProPlugin } from "vuepress-plugin-search-pro";
 import theme from "./theme.js";
 
 export default defineUserConfig({
-  base: "/",
+  base: "/study_blog/",
 
   lang: "zh-CN",
   title: "没有蜡笔的小新",
